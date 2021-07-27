6710a7ff45da40a5c59b971e793bd9c828a971ec
wdlord <44736323+wdlord@users.noreply.github.com>
2021-07-27 00:25:07 -0700

    update main for new OWM communication methods

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-from tomorrow_owm import Tomorrow
-import send_sms
+from Tomorrow import OWM
+import Send_SMS
 import schedule, time
+
 
 def rainy_day_bot():
 
-  # to run elsewhere, pass location as tuple, careful with NESW signs
-  # for example: tomorrow = Tomorrow(location=(33.4255, -111.9400)) is (N, W)
-  tomorrow = Tomorrow()
+  # check Tomorrow.py for optional OWM() arguments
+  tomorrow = OWM()
 
-  if tomorrow.precipitation_probability > 0.2:
+  if tomorrow.will_rain():
     forecast = tomorrow.forecast_string()
-    print(forecast)
-    send_sms.send_message("FORECAST ", forecast)
+    Send_SMS.send_message("FORECAST ", forecast)
+
+  # prints forecast and other useful info to console
+  tomorrow.debug_log()
 
 
 # MAIN program
+# --------------------------
 print("the bot has started...")
 
-# NOTE: this server runs on GMT Timezone, adjust scedules accordingly
+# for testing
+run_now = input("would you like to run the bot now? (y/n)")
+if run_now == 'y':
+  rainy_day_bot()
+
+# NOTE: this server runs on GMT Timezone, adjust schedules accordingly
 schedule.every().day.at("02:00").do(rainy_day_bot)
 
 # infinite loop
